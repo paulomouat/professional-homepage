36203751bd757b701431ac8321dd748124aa203f
Paulo Mouat <paulo.mouat@gmail.com>
2012-07-22 18:51:54 -0400

    Changed upload script to apply exclusions. Added Espresso project files.

--- upload.sh
+++ upload.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-tar -czf - *.* | ssh dmethods@dmethods.com "cd /home/dmethods/www/paulomouat; tar xzf -"
+tar -czf - --exclude-from=excludefiles.txt *.* | ssh dmethods@dmethods.com "cd /home/dmethods/www/paulomouat; tar xzf -"
